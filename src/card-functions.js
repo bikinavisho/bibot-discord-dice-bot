b8cbc6a05850ed046b43c6a6c331272633cbfaf7
Bianca <bikinavisho@yahoo.com>
2023-02-18 06:44:39 +0000

    archive: createDeckOfCardsArray

--- src/card-functions.js
+++ src/card-functions.js
@@ -10,3 +10,46 @@ const DECK_OF_CARDS = [
 module.exports = {
     DECK_OF_CARDS
 };
+
+function createDeckOfCardsArray() {
+    let a = [];
+    for (let i = 0; i < 4; i++) {
+        let cardSuit = '';
+        switch (i) {
+            case 0:
+                cardSuit = '♤';
+                break;
+            case 1:
+                cardSuit = '♡';
+                break;
+            case 2: 
+                cardSuit = '♢';
+                break;
+            case 3:
+                cardSuit = '♧';
+                break;
+        }
+        for (let j = 0; j < 13; j++) {
+            if (j == 0) {
+                a.push(cardSuit + 'A');
+            } else if (j < 10) {
+                a.push(cardSuit + String(j + 1));
+            } else {
+                switch (j) {
+                    case 10: 
+                        a.push(cardSuit + 'J');
+                        break;
+                    case 11: 
+                        a.push(cardSuit + 'Q');
+                        break;
+                    case 12: 
+                        a.push(cardSuit + 'K');
+                        break;
+                }
+            }
+        }
+        
+    }
+
+    return a;
+}
